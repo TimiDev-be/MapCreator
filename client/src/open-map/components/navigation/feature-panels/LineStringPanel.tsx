ec64d385d51f7c5301e42d34051a6c011de6e608
Tymoteusz Radziszewski <tymoteusz.radziszewskibusiness@gmail.com>
2026-06-12 17:47:39 +0200

    [Modified] Implementation for polygon and line string features panel

--- client/src/open-map/components/navigation/feature-panels/LineStringPanel.tsx
+++ client/src/open-map/components/navigation/feature-panels/LineStringPanel.tsx
@@ -1,10 +1,22 @@
 import { useMapContainer } from "../../../../shared/hooks/MapContainer";
 import { useLineFeature } from "../../../../shared/hooks/LineFeature";
+import CloseLogo from "../../../../assets/material-symbols_close.svg?react";
+import type { LineProperties } from "../../../../shared/types/LineProperties";
+import { useState } from "react";
 
 export default function LineStringPanel() {
   const { feature, toggleFeaturePanel } = useMapContainer();
-  const { name, color, lineWidth } = feature.properties;
-  const { handleColorChange, handleWidthChange } = useLineFeature();
+  const { name, color, lineWidth, lineDash } =
+    feature.properties as LineProperties;
+  const {
+    handleColorChange,
+    handleWidthChange,
+    handleLineDashChange,
+    toggleLineDash,
+  } = useLineFeature();
+  const [stateLineDash, setStateLineDash] = useState<number[]>(
+    lineDash ?? [3, 4],
+  );
 
   return (
     <>
@@ -14,7 +26,7 @@ export default function LineStringPanel() {
           className="close-feature-panel-button t-panel-medium"
           onClick={() => toggleFeaturePanel(null)}
         >
-          Close
+          <CloseLogo width={20} height={20} />
         </button>
         <p className="feature-name t-panel-medium">feature / {name}</p>
         <div className="group color">
@@ -47,6 +59,73 @@ export default function LineStringPanel() {
               onMouseUp={handleWidthChange}
             />
           </div>
+          <div className="wrapper">
+            <label htmlFor="is-dashed-checkbox" className="t-panel-small">
+              Dashed
+            </label>
+            <input
+              type="checkbox"
+              name="dashed"
+              id="is-dashed-checkbox"
+              className="panel-checkbox"
+              defaultChecked={lineDash ? true : false}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setStateLineDash([3, 4]);
+                } else {
+                  setStateLineDash(undefined);
+                }
+                toggleLineDash(e);
+              }}
+            />
+          </div>
+          {lineDash && stateLineDash && (
+            <div className="wrapper dash-gap">
+              <div className="field-container dash">
+                <label htmlFor="dash-input" className="t-panel-small">
+                  Dash
+                </label>
+                <input
+                  type="number"
+                  name="dash"
+                  id="dash-input"
+                  className="panel-field t-panel-small"
+                  min={0}
+                  defaultValue={(lineDash && lineDash[0]) ?? 3}
+                  onBlur={(e) => {
+                    let value = Number(e.currentTarget.value);
+                    if (value < 0) value = 0;
+                    e.currentTarget.value = value.toString();
+                    const LineDash: number[] = [value, stateLineDash[1]];
+                    setStateLineDash(LineDash);
+                    handleLineDashChange(LineDash);
+                  }}
+                />
+              </div>
+              <span className="field-bridge" />
+              <div className="field-container gap">
+                <label htmlFor="gap-input" className="t-panel-small">
+                  Gap
+                </label>
+                <input
+                  type="number"
+                  name="gap"
+                  id="gap-input"
+                  className="panel-field t-panel-small"
+                  min={0}
+                  defaultValue={(lineDash && lineDash[1]) ?? 4}
+                  onBlur={(e) => {
+                    let value = Number(e.currentTarget.value);
+                    if (value < 0) value = 0;
+                    e.currentTarget.value = value.toString();
+                    const LineDash: number[] = [stateLineDash[0], value];
+                    setStateLineDash(LineDash);
+                    handleLineDashChange(LineDash);
+                  }}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
--- client/src/open-map/components/navigation/feature-panels/PolygonPanel.tsx
+++ client/src/open-map/components/navigation/feature-panels/PolygonPanel.tsx
@@ -1,7 +1,74 @@
+import { useMapContainer } from "../../../../shared/hooks/MapContainer";
+import { usePolygonFeature } from "../../../../shared/hooks/PolygonFeature";
+import CloseLogo from "../../../../assets/material-symbols_close.svg?react";
+import type { PolygonProperties } from "../../../../shared/types/PolygonProperties";
+
 export default function PolygonPanel() {
+  const { feature, toggleFeaturePanel } = useMapContainer();
+  const { handleColorChange, handleWidthChange, handleBorderColorChange } =
+    usePolygonFeature();
+  const { name, color, lineWidth, borderColor } =
+    feature.properties as PolygonProperties;
+
   return (
     <>
-      <div className="feature-panel polygon">polygon</div>
+      <div className="feature-panel polygon">
+        <button
+          type="button"
+          className="close-feature-panel-button t-panel-medium"
+          onClick={() => toggleFeaturePanel(null)}
+        >
+          <CloseLogo width={20} height={20} />
+        </button>
+        <p className="feature-name t-panel-medium">feature / {name}</p>
+        <div className="group color">
+          <div className="wrapper">
+            <label htmlFor="feature-color-input" className="t-panel-small">
+              Background Color
+            </label>
+            <input
+              type="color"
+              id="feature-color-input"
+              name="feature-color"
+              className="color-input"
+              defaultValue={color}
+              onBlur={handleColorChange}
+            />
+          </div>
+          <div className="wrapper">
+            <label
+              htmlFor="feature-border-color-input"
+              className="t-panel-small"
+            >
+              Border Color
+            </label>
+            <input
+              type="color"
+              id="feature-border-color-input"
+              name="feature-border-color"
+              className="color-input"
+              defaultValue={borderColor}
+              onBlur={handleBorderColorChange}
+            />
+          </div>
+        </div>
+        <div className="group sizes">
+          <div className="wrapper">
+            <label htmlFor="range-width-input" className="t-panel-small">
+              Width ({lineWidth})
+            </label>
+            <input
+              type="range"
+              id="range-width-input"
+              name="range-width"
+              min="1"
+              max="12"
+              defaultValue={lineWidth}
+              onMouseUp={handleWidthChange}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
