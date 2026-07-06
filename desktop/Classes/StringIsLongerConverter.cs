774940385f0cf56b027c10a87fbc8da41b4562a8
Tymoteusz Radziszewski <tymoteusz.radziszewskibusiness@gmail.com>
2026-07-06 21:22:47 +0200

    [Add / Modified] New images, converter, styles class modification Converter is for handling too long string on ui side Style class modifications are about propertychanged notification on ui

---
+++ desktop/Classes/StringIsLongerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace desktop.Classes
+{
+    public class StringIsLongerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = (string)value ?? "";
+            var limited = parameter is not null ? int.Parse(parameter.ToString() ?? "100") : 100;
+            return text.Length > limited ? text[..limited] + "..." : text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- desktop/Classes/Style.cs
+++ desktop/Classes/Style.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace desktop.Classes
 {
-    public class Style(string Name, string Url)
+    public class Style(string Name, string Url) : INotifyPropertyChanged
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = Name;
         public string Url { get; set; } = Url;
-        public bool IsActive { get; set; } = false;
+        private bool _isActive { get; set; } = false;
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                _isActive = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsActive)));
+            }
+
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
