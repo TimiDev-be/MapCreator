e4709c0fcf4a0356e9dbd921826ed747108a0cdc
Tymoteusz Radziszewski <tymoteusz.radziszewskibusiness@gmail.com>
2026-07-24 20:21:27 +0200

    [Add] App errors

---
+++ desktop/Errors/AETemplateBodyRequired.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desktop.Errors
+{
+    public class AETemplateBodyRequired : AppError
+    {
+        public AETemplateBodyRequired() : base("Template body is required.")
+        {
+            
+        }
+    }
+}
---
+++ desktop/Errors/AppError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desktop.Errors
+{
+    public class AppError : Exception
+    {
+        public string Title { get; set; } = "Something went wrong";
+        public string Description { get; set; } = string.Empty;
+        public int StatusCode { get; set; } = 400;
+
+        public AppError(string description) : base(description)
+        {
+            this.Description = description;
+        }
+    }
+}
